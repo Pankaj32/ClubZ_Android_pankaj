a1b45411750340ffdbb7393a1b289b0107409396
chiranjib <brajmohan.mindiii@gmail.com>
2018-07-13 16:07:38 +0530

    fri 13 july work

--- app/src/main/java/com/clubz/chat/fragments/FragmentChat.kt
+++ app/src/main/java/com/clubz/chat/fragments/FragmentChat.kt
@@ -1,15 +1,23 @@
 package com.clubz.chat.fragments
 
+import android.Manifest
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
+import android.support.v4.content.FileProvider
+import android.support.v7.widget.PopupMenu
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
+import com.clubz.BuildConfig
 import com.clubz.ClubZ
 
 import com.clubz.R
@@ -17,12 +25,22 @@ import com.clubz.chat.adapter.ChatRecyclerAdapter
 import com.clubz.chat.model.ChatBean
 import com.clubz.chat.model.FeedBean
 import com.clubz.chat.util.ChatUtil
+import com.clubz.utils.Constants
+import com.clubz.utils.cropper.CropImage
+import com.clubz.utils.cropper.CropImageView
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.FirebaseApp
 import com.google.firebase.database.*
 import com.google.firebase.iid.FirebaseInstanceId
 import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.OnProgressListener
 import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
+import com.mvc.imagepicker.ImagePicker
 import kotlinx.android.synthetic.main.fragment_chat.*
+import java.io.File
+import java.io.IOException
 
 import java.util.ArrayList
 
@@ -54,7 +72,8 @@ class FragmentChat : Fragment(), View.OnClickListener {
     private var chatRoom = ""
     private val databaseReference = FirebaseDatabase.getInstance().reference
     private var mChatRecyclerAdapter: ChatRecyclerAdapter? = null
-
+    private var isCameraSelected: Boolean = false
+    private var imageUri: Uri? = null
 
     private var mListener: OnFragmentInteractionListener? = null
 
@@ -87,6 +106,7 @@ class FragmentChat : Fragment(), View.OnClickListener {
             }
         }
         sentButton.setOnClickListener(this)
+        sendPicBtn.setOnClickListener(this)
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -116,12 +136,14 @@ class FragmentChat : Fragment(), View.OnClickListener {
     override fun onClick(p0: View?) {
         when (p0?.id) {
             R.id.sentButton -> {
-
                 if (txtMsg.text.toString().isNotEmpty()) {
                     sendMessage(txtMsg.text.toString(), "text", chatFor)
                 } else {
                     Toast.makeText(mContext, R.string.please_type, Toast.LENGTH_LONG).show()
                 }
+            }
+            R.id.sendPicBtn -> {
+                permissionPopUp()
             }
         }
     }
@@ -198,7 +220,7 @@ class FragmentChat : Fragment(), View.OnClickListener {
                         val feedsBean = dataSnapshot?.getValue(FeedBean::class.java)
                         if (feedsBean?.isCommentAllow.equals("1")) {
                             getMessageFromFirebaseUser()
-                        }else{
+                        } else {
                             txtMsg.setFocusable(false)
                             txtMsg.setText("Comment disable")
                         }
@@ -307,5 +329,153 @@ class FragmentChat : Fragment(), View.OnClickListener {
             fragment.arguments = args
             return fragment
         }
+    }
+
+    fun permissionPopUp() {
+        val wrapper = ContextThemeWrapper(mContext, R.style.popstyle)
+        val popupMenu = PopupMenu(wrapper, sendPicBtn, Gravity.CENTER)
+        popupMenu.getMenuInflater().inflate(R.menu.popupmenu, popupMenu.getMenu())
+        popupMenu.setOnMenuItemClickListener(object : PopupMenu.OnMenuItemClickListener {
+            override fun onMenuItemClick(item: MenuItem): Boolean {
+                isCameraSelected = true
+                when (item.getItemId()) {
+                    R.id.pop1 -> if (Build.VERSION.SDK_INT >= 23) {
+                        if (mContext?.checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                            callIntent(Constants.INTENTREQUESTCAMERA)
+                        } else if (mContext?.checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                            callIntent(Constants.INTENTREQUESTREAD)
+                        } else {
+                            callIntent(Constants.INTENTCAMERA)
+                        }
+                    } else {
+                        callIntent(Constants.INTENTCAMERA)
+                    }
+                    R.id.pop2 -> if (Build.VERSION.SDK_INT >= 23) {
+                        isCameraSelected = false
+                        if (mContext?.checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                            callIntent(Constants.INTENTREQUESTREAD)
+                        } else {
+                            callIntent(Constants.INTENTGALLERY)
+                        }
+                    } else {
+                        callIntent(Constants.INTENTGALLERY)
+                    }
+                }
+                return false
+            }
+        })
+        popupMenu.show()
+    }
+
+
+    fun callIntent(caseid: Int) {
+
+        when (caseid) {
+            Constants.INTENTCAMERA -> {
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                var file = File(Environment.getExternalStorageDirectory().toString() + File.separator + "image.jpg");
+                imageUri =
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                            FileProvider.getUriForFile(mContext, BuildConfig.APPLICATION_ID + ".provider", file)
+                        } else {
+                            Uri.fromFile(file)
+                        }
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);//USE file code in_ this case
+                startActivityForResult(intent, Constants.REQUEST_CAMERA)
+            }
+            Constants.INTENTGALLERY -> {
+                ImagePicker.pickImage(this);
+                // com.clubz.utils.picker.ImagePicker.pickImage(this@NewActivities)
+            }
+            Constants.INTENTREQUESTCAMERA -> ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE),
+                    Constants.MY_PERMISSIONS_REQUEST_CAMERA)
+            Constants.INTENTREQUESTREAD -> ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    Constants.MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE)
+            Constants.INTENTREQUESTWRITE -> {
+            }
+
+            Constants.INTENTREQUESTNET -> {
+                ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.INTERNET),
+                        Constants.MY_PERMISSIONS_REQUEST_INTERNET)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+
+        when (requestCode) {
+            Constants.MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (!isCameraSelected) callIntent(Constants.INTENTGALLERY)
+                } else {
+                    Toast.makeText(mContext, R.string.a_permission_denied, Toast.LENGTH_LONG).show()
+                }
+            }
+            Constants.MY_PERMISSIONS_REQUEST_CAMERA -> if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (isCameraSelected) callIntent(Constants.INTENTCAMERA)
+            } else {
+                Toast.makeText(mContext, R.string.a_camera_denied, Toast.LENGTH_LONG).show()
+            }
+            Constants.MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE -> if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (!isCameraSelected) callIntent(Constants.INTENTGALLERY) else callIntent(Constants.INTENTCAMERA)
+            } else {
+                Toast.makeText(mContext, R.string.a_permission_read, Toast.LENGTH_LONG).show()
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == -1) {
+            if (requestCode == Constants.SELECT_FILE) {
+                imageUri = com.clubz.utils.picker.ImagePicker.getImageURIFromResult(mContext, requestCode, resultCode, data);
+                if (imageUri != null) {
+                    CropImage.activity(imageUri).setCropShape(CropImageView.CropShape.RECTANGLE).setMinCropResultSize(300, 200).setMaxCropResultSize(4000, 4000).setAspectRatio(300, 200).start(mContext!!, this)
+                } else {
+                    Toast.makeText(activity, R.string.swr, Toast.LENGTH_SHORT).show()
+                }
+            }
+            if (requestCode == Constants.REQUEST_CAMERA) {
+                // val imageUri :Uri= com.tulia.Picker.ImagePicker.getImageURIFromResult(this, requestCode, resultCode, data);
+                if (imageUri != null) {
+                    CropImage.activity(imageUri).setCropShape(CropImageView.CropShape.RECTANGLE).setMinCropResultSize(300, 200).setMaxCropResultSize(4000, 4000).setAspectRatio(300, 200).start(mContext!!, this)
+                } else {
+                    Toast.makeText(mContext, R.string.swr, Toast.LENGTH_SHORT).show()
+                }
+            }
+            if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+                var result: CropImage.ActivityResult = CropImage.getActivityResult(data)
+                try {
+                    if (result != null)
+                        sendFileFireBase(result.uri)
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        isCameraSelected = false
+    }
+
+    private fun sendFileFireBase(selectedImageUri: Uri) {
+        progressbar.visibility = View.VISIBLE
+        storageRef = mstorage?.getReference("chat_photos_" + getString(R.string.app_name))
+        val photoRef = storageRef?.child(selectedImageUri.lastPathSegment)
+        photoRef?.putFile(selectedImageUri)?.addOnSuccessListener { taskSnapshot ->
+            val fireBaseUri = taskSnapshot.downloadUrl
+            Log.e("TAG", "onSuccess: ")
+            progressbar.visibility = View.GONE
+            sendMessage(fireBaseUri!!.toString(), "image", chatFor)
+        }
+                ?.addOnFailureListener { e ->
+                    progressbar.visibility = View.GONE
+                    Log.e("TAG", "onFailure: " + e.message)
+                    Toast.makeText(mContext, "Failed " + e.message, Toast.LENGTH_SHORT).show()
+                }
+                ?.addOnProgressListener { taskSnapshot ->
+                    val progress = 100.0 * taskSnapshot.bytesTransferred / taskSnapshot.totalByteCount
+                    Log.e("TAG", "onProgress: $progress")
+                }
     }
 }// Required empty public constructor
